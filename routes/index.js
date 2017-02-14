368563ab868d7ff6e9d114d673e22a2d225e64e5
faridmovsumov <faridmovsumov@gmail.com>
2017-02-14 18:50:01 +0200

    username validation

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+
+  if(isBlank(req.query.username)){
+    res.writeHead(301,
+        {Location: '/'}
+    );
+    res.end();
+  }
+
   res.render('index', {"username": req.query.username});
 });
+
+function isBlank(str) {
+  return (!str || /^\s*$/.test(str));
+}
 
 module.exports = router;
